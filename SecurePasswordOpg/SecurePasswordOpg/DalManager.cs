78e57683921e039650fa279883562e9ad0611714
mortenvestergaard <57791299+mortenvestergaard@users.noreply.github.com>
2021-01-06 17:39:54 +0100

    Add files via upload

---
+++ SecurePasswordOpg/SecurePasswordOpg/DalManager.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurePasswordOpg
+{
+    class DalManager
+    {
+        private static string connString = "Server=(localdb)\\MSSQLLocaldb; Database=SecurePassword; Integrated Security=True";
+
+        private static string salt;
+
+        public void GenerateSalt()
+        {
+            using (var randomGenerator = new RNGCryptoServiceProvider())
+            {
+                byte[] buff = new byte[8];
+                randomGenerator.GetBytes(buff);
+
+                salt = Convert.ToBase64String(buff);
+            }
+        }
+
+        public string CreateHashedPassword(string passwrd)
+        {
+            byte[] pwdWithSalt = Encoding.ASCII.GetBytes(string.Concat(passwrd, salt));
+            using (var sha256 = SHA256.Create())
+            {
+                return Convert.ToBase64String(sha256.ComputeHash(pwdWithSalt));
+            }
+        }
+
+        public void StoreHashedPassword(string username, string hashedPassword)
+        {
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("INSERT INTO Users VALUES (@username, @password, @salt)", conn);
+                cmd.Parameters.Add(new SqlParameter("@username", username));
+                cmd.Parameters.Add(new SqlParameter("@password", hashedPassword));
+                cmd.Parameters.Add(new SqlParameter("@salt", salt));
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public static string GetSaltFromDatabase(string username)
+        {
+            string returnedSalt = "";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Salt FROM Users WHERE Username = @username", conn);
+                cmd.Parameters.Add(new SqlParameter("@username", username));
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    returnedSalt = (string)rdr[0];
+                }
+            }
+            return returnedSalt;
+        }
+
+        public static string GetHashedPasswordFromDataBase(string username)
+        {
+            string returnedPwd = "";
+            using (SqlConnection conn = new SqlConnection(connString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("SELECT Password FROM Users WHERE Username = @username", conn);
+                cmd.Parameters.Add(new SqlParameter("@username", username));
+                SqlDataReader rdr = cmd.ExecuteReader();
+                while (rdr.Read())
+                {
+                    returnedPwd = (string)rdr[0];
+                }
+            }
+            return returnedPwd;
+        }
+        public bool ValidatePassword(string password, string username)
+        {
+            string tempPwd = "";
+            byte[] pwdWithSaltFromDB = Encoding.ASCII.GetBytes(string.Concat(password, GetSaltFromDatabase(username)));
+            using (var sha256 = SHA256.Create())
+            {
+                tempPwd = Convert.ToBase64String(sha256.ComputeHash(pwdWithSaltFromDB));
+            }
+            if (tempPwd == GetHashedPasswordFromDataBase(username))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+    }
+}
---
+++ SecurePasswordOpg/SecurePasswordOpg/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurePasswordOpg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DalManager dalManager = new DalManager();
+            Console.WriteLine("Please enter your username and password to login");
+            Console.WriteLine("(1) Create an account");
+            Console.WriteLine("(2) Login");
+            int input = int.Parse(Console.ReadLine());
+            while (true)
+            {
+
+                switch (input)
+                {
+                    case 1:
+                        Console.WriteLine("Write wanted username:");
+                        string newUsername = Console.ReadLine();
+                        Console.WriteLine("Write wanted password:");
+                        string newPassword = Console.ReadLine();
+                        dalManager.GenerateSalt();
+                        dalManager.StoreHashedPassword(newUsername, dalManager.CreateHashedPassword(newPassword));
+                        Console.WriteLine("user created");
+                        break;
+                    case 2:
+                        Console.WriteLine("Username:");
+                        string loginUsername = Console.ReadLine();
+                        Console.WriteLine("Password:");
+                        string loginPassword = Console.ReadLine();
+                        if (dalManager.ValidatePassword(loginPassword, loginUsername) == true)
+                        {
+                            Console.WriteLine("access granted");
+                        }
+                        else
+                        {
+                            Console.WriteLine("access denied");
+                        }
+
+                        break;
+                    default:
+                        Console.WriteLine("Wrong input");
+                        break;
+                }
+            }
+        }
+    }
+}
